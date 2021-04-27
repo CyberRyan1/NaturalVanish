524f27e14266ff977aaaba1fa0c834243a6d8824
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2021-04-27 18:11:04 -0500

    Redid entire ConfigManager class

--- src/main/java/com/github/cyberryan1/managers/ConfigManager.java
+++ src/main/java/com/github/cyberryan1/managers/ConfigManager.java
@@ -1,36 +1,73 @@
 package com.github.cyberryan1.managers;
 
 import com.github.cyberryan1.NaturalVanish;
-
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
 
 public class ConfigManager {
 
-    private NaturalVanish plugin;
-    private FileConfiguration config = null;
+    public NaturalVanish plugin;
+
+    private FileConfiguration dataConfig;
+    private File configFile;
+
 
     public ConfigManager( NaturalVanish pl ) {
-        plugin = pl;
+        this.plugin = pl;
+        saveDefaultConfig();
     }
 
 
 
     public void reloadConfig() {
-        if ( config == null ) {
-            config = plugin.getConfig();
+        if ( this.configFile == null ) {
+            this.configFile = new File( this.plugin.getDataFolder(), "config.yml" );
         }
 
-        plugin.saveDefaultConfig();
-        plugin.reloadConfig();
+        this.dataConfig = YamlConfiguration.loadConfiguration( this.configFile );
+
+        InputStream defaultStream = this.plugin.getResource( "config.yml" );
+        if ( defaultStream != null ) {
+            YamlConfiguration defaultConfig = YamlConfiguration.loadConfiguration( new InputStreamReader( defaultStream ) );
+            this.dataConfig.setDefaults( defaultConfig );
+        }
     }
 
 
 
     public FileConfiguration getConfig() {
-        if ( config == null ) {
+        if ( this.dataConfig == null ) {
             reloadConfig();
         }
 
-        return config;
+        return this.dataConfig;
+    }
+
+
+
+    public void saveConfig() {
+        if ( this.dataConfig == null || this.configFile == null ) {
+            plugin.getLogger().log( Level.WARNING, "dataConfig == null || configFile == null, returning " );
+            return;
+        }
+
+        plugin.saveConfig();
+    }
+
+
+
+    public void saveDefaultConfig() {
+        if ( this.configFile == null ) {
+            this.configFile = new File( this.plugin.getDataFolder(), "config.yml" );
+        }
+
+        if ( ! this.configFile.exists() ) {
+            this.plugin.saveResource( "config.yml", false );
+        }
     }
 }
