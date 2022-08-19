ad11c145c0e8b885421c33e06077f2860a942388
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-08-19 01:14:22 -0500

    Initialize CyberCore

--- src/main/java/com/github/cyberryan1/NaturalVanish.java
+++ src/main/java/com/github/cyberryan1/NaturalVanish.java
@@ -2,27 +2,18 @@ package com.github.cyberryan1;
 
 import ch.njol.skript.Skript;
 import ch.njol.skript.SkriptAddon;
+import com.github.cyberryan1.cybercore.CyberCore;
+import com.github.cyberryan1.cybercore.utils.CoreUtils;
+import com.github.cyberryan1.cybercore.utils.VaultUtils;
 import com.github.cyberryan1.events.*;
-import com.github.cyberryan1.managers.ConfigManager;
-import com.github.cyberryan1.managers.DataManager;
 import com.github.cyberryan1.skriptelements.conditions.RegisterConditions;
-import com.github.cyberryan1.utils.*;
-
+import com.github.cyberryan1.utils.BossbarUtils;
+import com.github.cyberryan1.utils.yml.YMLUtils;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.IOException;
 
 public final class NaturalVanish extends JavaPlugin {
-
-    // config/data managers
-    public ConfigManager config;
-    public DataManager data;
-
-    // utilities that need to be initialized
-    public Utilities util;
-    public ConfigUtils configUtils;
-    public DataUtils dataUtils;
-    public VaultUtils vaultUtils;
 
     // Skript
     public SkriptAddon addon;
@@ -30,17 +21,44 @@ public final class NaturalVanish extends JavaPlugin {
     
     @Override
     public void onEnable() {
-        config = new ConfigManager( this );
-        data = new DataManager( this );
+        // Initialize CyberCore
+        CyberCore.setPlugin( this );
+        new VaultUtils();
 
-        util = new Utilities( this, config, data );
-        configUtils = new ConfigUtils( config );
-        dataUtils = new DataUtils( data );
-        vaultUtils = new VaultUtils();
+        // Update/reload config/data files
+        YMLUtils.getConfig().getYMLManager().initialize();
+        YMLUtils.getData().getYMLManager().initialize();
+        YMLUtils.getData().sendPathNotFoundWarns( false );
 
+        // Remove all bossbars
+        BossbarUtils.removeAllBossbars();
+
+        // Attempt to register Skript syntax
+        try {
+            addon = Skript.registerAddon( this );
+            try {
+                addon.loadClasses( "com.github.cyberryan1", "skriptelements" );
+            } catch ( IOException e ) {
+                CoreUtils.logWarn( "Could not enable as a skript addon, will still enable without this syntax!" );
+                enabled = false;
+            }
+            CoreUtils.logInfo( "Enabled as a skript addon" );
+            RegisterConditions.register();
+        } catch ( NoClassDefFoundError error ) {
+            CoreUtils.logWarn( "Could not enable as a skript addon, will still enable without this syntax!" );
+            enabled = false;
+        }
+
+        loadCommands();
+        loadEvents();
+    }
+
+    private void loadCommands() {
         this.getCommand( "vanish" ).setExecutor( new Vanish() );
         this.getCommand( "vanish" ).setTabCompleter( new TabComplete() );
+    }
 
+    private void loadEvents() {
         this.getServer().getPluginManager().registerEvents( new InventoryClick(), this );
         this.getServer().getPluginManager().registerEvents( new Join(), this );
         this.getServer().getPluginManager().registerEvents( new Leave(), this );
@@ -52,26 +70,5 @@ public final class NaturalVanish extends JavaPlugin {
         this.getServer().getPluginManager().registerEvents( new Damage(), this );
         this.getServer().getPluginManager().registerEvents( new DamageByEntity(), this );
         this.getServer().getPluginManager().registerEvents( new Hunger(), this );
-
-        ConfigUtils.getConfigManager().reloadConfig();
-
-        // Remove all bossbars
-        BossbarUtils.removeAllBossbars();
-
-        // Attempt to register Skript syntax
-        try {
-            addon = Skript.registerAddon( this );
-            try {
-                addon.loadClasses( "com.github.cyberryan1", "skriptelements" );
-            } catch ( IOException e ) {
-                Utilities.logWarn( "Could not enable as a skript addon, will still enable without this syntax!" );
-                enabled = false;
-            }
-            Utilities.logInfo( "Enabled as a skript addon" );
-            RegisterConditions.register();
-        } catch ( NoClassDefFoundError error ) {
-            Utilities.logWarn( "Could not enable as a skript addon, will still enable without this syntax!" );
-            enabled = false;
-        }
     }
 }
